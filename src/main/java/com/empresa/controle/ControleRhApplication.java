624dfeec4c9ab4538e4f4c10bf17e083fb4a8abe
Lucas Duarte <lucasduartedev@gmail.com>
2022-05-03 08:25:51 -0300

    Início do projeto

---
+++ src/main/java/com/empresa/controle/ControleRhApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.controle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleRhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresa/controle/ControleRhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.controle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
